300c02c918a02af881dd6e26685281cfde67c246
sourish115 <sourish115@live.com>
2020-01-24 12:58:22 +0530

    AddItem Module Completed

--- src/app/components/seller-side/menu-item/menu-item.component.ts
+++ src/app/components/seller-side/menu-item/menu-item.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, AfterViewInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, Form} from '@angular/forms';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import { SellerItem } from 'src/app/entities/seller-item.entity';
 
@@ -17,9 +17,11 @@ import { SellerItem } from 'src/app/entities/seller-item.entity';
 export class MenuItemComponent implements OnInit {
   ItemDetailsFormGroup : FormGroup;
   ItemPropertiesFormGroup : FormGroup;
+  ItemPriceTimeFormGroup : FormGroup;
   characters_used : number = 0;
   characters_left: number = 60;
   newSellerItemObject : SellerItem;
+  itemIsVeg : boolean = true;
   categories = [
     {name:'Indian'},
     {name:'Western'},
@@ -35,6 +37,7 @@ export class MenuItemComponent implements OnInit {
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    console.log(this.itemIsVeg);
     this.ItemDetailsFormGroup = this._formBuilder.group({
       Item_Name : ['',[
         Validators.required,
@@ -53,16 +56,32 @@ export class MenuItemComponent implements OnInit {
       Item_Category : ['',[
         Validators.required
       ]],
-      Item_VegorNonveg:['',[
-        Validators.required
-      ]],
+      Item_VegorNonveg:[''],
     });
+    this.ItemPriceTimeFormGroup = this._formBuilder.group({
+      Item_Price:['',[
+        Validators.required,
+        Validators.pattern(/^[0-9.]+$/)
+      ]],
+      Item_Preparation_Time:['',[
+        Validators.required
+      ]]
+    })
   }
   addToFormObject(data){
     let ItemObjtoPush = [];
-    ItemObjtoPush.push(data)
+    ItemObjtoPush.push(data);
+    console.log(ItemObjtoPush);
+  }
+  getVegNonVegValue(){
+    this.itemIsVeg = !this.itemIsVeg;
+    console.log(this.itemIsVeg);
   }
   get Item_Name() { return this.ItemDetailsFormGroup.get("Item_Name");}
   get Item_Image() { return this.ItemDetailsFormGroup.get("Item_Image");}
   get Item_Desc() { return this.ItemDetailsFormGroup.get("Item_Desc");}
+  get Item_Category(){ return this.ItemPropertiesFormGroup.get("Item_Category");}
+  get Item_VegorNonveg(){ return this.ItemPropertiesFormGroup.get("this.Item_VegorNonveg");}
+  get Item_Price(){ return this.ItemPriceTimeFormGroup.get("Item_Price");}
+  get Item_Preparation_Time(){ return this.ItemPriceTimeFormGroup.get("Item_Preparation_Time");}
 }
