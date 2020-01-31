13e6ebc001445edb71285c8bd632c2af3b8fdeba
Rahul <rahul.doit53@gmail.com>
2020-01-31 15:53:53 +0530

    Seller dashboard under construction

--- src/app/components/seller-side/seller-dashboard/seller-dashboard.component.ts
+++ src/app/components/seller-side/seller-dashboard/seller-dashboard.component.ts
@@ -11,98 +11,92 @@ import { Router } from '@angular/router';
   styleUrls: ['./seller-dashboard.component.css']
 })
 export class SellerDashboardComponent implements OnInit {
-  sellerData$ : Observer<any>;
-  sellerDashOptions : SellerDash; 
-  sellerCompletedOrders : [];
-  sellerRequestedOrders : [];
+  sellerData$: Observer<any>;
+  sellerDashOptions: SellerDash;
+  sellerCompletedOrders: [];
+  sellerRequestedOrders: [];
   sellerActiveOrders: [];
-  sellerItemsForDashboard$ : Subscription;
-  sellerCompletedOrdersForDashboard$ : Observer<any>;
-  sellerRequestedOrdersForDashboard$ : Observer<any>; 
-  sellerAcceptedOrdersForDashboard$ : Observer<any>;
-  constructor(private api : ApiService,
-    private router : Router) { 
+  sellerItemsForDashboard$: Subscription;
+  sellerCompletedOrdersForDashboard$: Observer<any>;
+  sellerRequestedOrdersForDashboard$: Observer<any>;
+  sellerAcceptedOrdersForDashboard$: Observer<any>;
+  constructor(private api: ApiService,
+    private router: Router) {
   }
   ngOnInit() {
-   this.sellerData$ = {
-     next : (data) => this.sellerDashOptions = data,
-     error : (err) => console.log(err),
-     complete : () => console.log("Request completed")
-   };
-   this.sellerCompletedOrdersForDashboard$ = {
-     next : (data) => this.sellerCompletedOrders = data,
-     error: (err) => console.log(err),
-     complete : () => console.log("Request for seller orders completed")
-   }
-   this.sellerRequestedOrdersForDashboard$ = {
-     next : (data) => this.sellerRequestedOrders = data,
-     error : (err) => console.log(err),
-     complete : () => console.log("Request for requested orders completed")
-   }
-   this.sellerAcceptedOrdersForDashboard$  = {
-    next : (data) => this.sellerActiveOrders = data,
-    error : (err) => console.log(err),
-    complete : () => console.log("Request for active orders completed")
-  }
-   this.sellerDashboardRequestedOrders();
-   this.sellerDashboardActiveOrders();  
-   this.sellerDashboardCompletedOrders();
-   this.sellerQuickdataForDashboard();
+    this.sellerData$ = {
+      next: (data) => this.sellerDashOptions = data,
+      error: (err) => console.log(err),
+      complete: () => console.log("Request completed")
+    };
+    this.sellerCompletedOrdersForDashboard$ = {
+      next: (data) => this.sellerCompletedOrders = data,
+      error: (err) => console.log(err),
+      complete: () => console.log("Request for seller orders completed")
+    }
+    this.sellerRequestedOrdersForDashboard$ = {
+      next: (data) => { this.sellerRequestedOrders = data; console.log(this.sellerRequestedOrders); },
+      error: (err) => console.log(err),
+      complete: () => console.log("Request for requested orders completed")
+    }
+    this.sellerAcceptedOrdersForDashboard$ = {
+      next: (data) => this.sellerActiveOrders = data,
+      error: (err) => console.log(err),
+      complete: () => console.log("Request for active orders completed")
+    }
+    this.sellerDashboardRequestedOrders();
+    this.sellerDashboardActiveOrders();
+    this.sellerDashboardCompletedOrders();
+    this.sellerQuickdataForDashboard();
     this.sellerItemsForDashboard$ = this.api.getSellerDashboardMeals()
-    .subscribe(
-      (data) =>{
-        console.log(data);
-        data;
-      },
-      (error) => console.log(error),
-      () => console.log("Fetch Meals Completed")
-    )
+      .subscribe(
+        (data) => {
+          console.log(data);
+          data;
+        },
+        (error) => console.log(error),
+        () => console.log("Fetch Meals Completed")
+      )
     this.api.getSellerDashboardMealById()
-    .subscribe(
-      (data) => console.log(data),
-      (err) => console.log(err),
-      () => console.log("Fetched Single Meal data Request Completed")
-    )
+      .subscribe(
+        (data) => console.log(data),
+        (err) => console.log(err),
+        () => console.log("Fetched Single Meal data Request Completed")
+      )
   }
-  sellerDashboardRequestedOrders(){
-    this.api.getSellerDashboardOrders(true,false,5).subscribe(this.sellerRequestedOrdersForDashboard$);
+  sellerDashboardRequestedOrders() {
+    this.api.getSellerDashboardOrders(true, false, 5).subscribe(this.sellerRequestedOrdersForDashboard$);
   }
-  sellerDashboardActiveOrders(){
-    this.api.getSellerDashboardOrders(true,true).subscribe(this.sellerAcceptedOrdersForDashboard$);
+  sellerDashboardActiveOrders() {
+    this.api.getSellerDashboardOrders(true, true).subscribe(this.sellerAcceptedOrdersForDashboard$);
   }
-  sellerDashboardCompletedOrders()
-  {
-    this.api.getSellerDashboardOrders(false,false,4).subscribe(this.sellerCompletedOrdersForDashboard$);
+  sellerDashboardCompletedOrders() {
+    this.api.getSellerDashboardOrders(false, false, 4).subscribe(this.sellerCompletedOrdersForDashboard$);
   }
-  sellerQuickdataForDashboard()
-  {
+  sellerQuickdataForDashboard() {
     this.api.getSellerQuickData().subscribe(this.sellerData$);
   }
-  sellerDashboardorderAction(orderId,status){
-    this.api.modifyRequestedOrderStatusById(orderId,status).subscribe(
+  sellerDashboardorderAction(orderId, status) {
+    this.api.modifyRequestedOrderStatusById(orderId, status).subscribe(
       (data) => console.log(data),
       (err) => console.log(err),
-      () => 
-      {
-        if(status === 1) 
-        {
+      () => {
+        if (status === 1) {
           this.sellerDashboardRequestedOrders();
-          this.sellerDashboardActiveOrders(); 
+          this.sellerDashboardActiveOrders();
         }
-        else if(status === 4)
-        {
-          this.sellerDashboardActiveOrders(); 
+        else if (status === 4) {
+          this.sellerDashboardActiveOrders();
           this.sellerDashboardCompletedOrders();
           this.sellerQuickdataForDashboard();
         }
-        else this.sellerDashboardActiveOrders(); 
+        else this.sellerDashboardActiveOrders();
         console.log("Order Status Modified");
       }
     );
   }
-  updateRequestedOrderStatus(orderId : number,status : number){
-    if(status === 6)
-    {
+  updateRequestedOrderStatus(orderId: number, status: number) {
+    if (status === 6) {
       this.api.rejectRequestedOrderStatusById(orderId).subscribe(
         (data) => console.log(data),
         (err) => console.log(err),
@@ -112,6 +106,12 @@ export class SellerDashboardComponent implements OnInit {
         }
       );
     }
-    else this.sellerDashboardorderAction(orderId,status);
+    else this.sellerDashboardorderAction(orderId, status);
+  }
+  getRemainingItems(id: Number) {
+    let order : any;
+    for(order of this.sellerRequestedOrders){
+      if(order.id === id) return order.items.length - 2; 
+    }
   }
 }
--- src/app/services/seller-dashboard-service/seller-dashboard.service.ts
+++ src/app/services/seller-dashboard-service/seller-dashboard.service.ts
@@ -1,9 +1,115 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Observer, Subscription } from 'rxjs';
+import { SellerDash } from 'src/app/entities/seller-dash.entity';
+import { ApiService } from '../api-service/api.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SellerDashboardService {
+export class SellerDashboardService implements OnInit{
 
-  constructor() { }
+  sellerData$ : Observer<any>;
+  sellerDashOptions : SellerDash; 
+  sellerCompletedOrders : [];
+  sellerRequestedOrders : [];
+  sellerActiveOrders: [];
+  sellerItemsForDashboard$ : Subscription;
+  sellerCompletedOrdersForDashboard$ : Observer<any>;
+  sellerRequestedOrdersForDashboard$ : Observer<any>; 
+  sellerAcceptedOrdersForDashboard$ : Observer<any>;
+  constructor(private api : ApiService,
+    private router : Router) { 
+  }
+  ngOnInit() {
+   this.sellerData$ = {
+     next : (data) => this.sellerDashOptions = data,
+     error : (err) => console.log(err),
+     complete : () => console.log("Request completed")
+   };
+   this.sellerCompletedOrdersForDashboard$ = {
+     next : (data) => this.sellerCompletedOrders = data,
+     error: (err) => console.log(err),
+     complete : () => console.log("Request for seller orders completed")
+   }
+   this.sellerRequestedOrdersForDashboard$ = {
+     next : (data) => this.sellerRequestedOrders = data,
+     error : (err) => console.log(err),
+     complete : () => console.log("Request for requested orders completed")
+   }
+   this.sellerAcceptedOrdersForDashboard$  = {
+    next : (data) => this.sellerActiveOrders = data,
+    error : (err) => console.log(err),
+    complete : () => console.log("Request for active orders completed")
+  }
+   this.sellerDashboardRequestedOrders();
+   this.sellerDashboardActiveOrders();  
+   this.sellerDashboardCompletedOrders();
+   this.sellerQuickdataForDashboard();
+    this.sellerItemsForDashboard$ = this.api.getSellerDashboardMeals()
+    .subscribe(
+      (data) =>{
+        console.log(data);
+        data;
+      },
+      (error) => console.log(error),
+      () => console.log("Fetch Meals Completed")
+    )
+    this.api.getSellerDashboardMealById()
+    .subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err),
+      () => console.log("Fetched Single Meal data Request Completed")
+    )
+  }
+  sellerDashboardRequestedOrders(){
+    this.api.getSellerDashboardOrders(true,false,5).subscribe(this.sellerRequestedOrdersForDashboard$);
+  }
+  sellerDashboardActiveOrders(){
+    this.api.getSellerDashboardOrders(true,true).subscribe(this.sellerAcceptedOrdersForDashboard$);
+  }
+  sellerDashboardCompletedOrders()
+  {
+    this.api.getSellerDashboardOrders(false,false,4).subscribe(this.sellerCompletedOrdersForDashboard$);
+  }
+  sellerQuickdataForDashboard()
+  {
+    this.api.getSellerQuickData().subscribe(this.sellerData$);
+  }
+  sellerDashboardorderAction(orderId,status){
+    this.api.modifyRequestedOrderStatusById(orderId,status).subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err),
+      () => 
+      {
+        if(status === 1) 
+        {
+          this.sellerDashboardRequestedOrders();
+          this.sellerDashboardActiveOrders(); 
+        }
+        else if(status === 4)
+        {
+          this.sellerDashboardActiveOrders(); 
+          this.sellerDashboardCompletedOrders();
+          this.sellerQuickdataForDashboard();
+        }
+        else this.sellerDashboardActiveOrders(); 
+        console.log("Order Status Modified");
+      }
+    );
+  }
+  updateRequestedOrderStatus(orderId : number,status : number){
+    if(status === 6)
+    {
+      this.api.rejectRequestedOrderStatusById(orderId).subscribe(
+        (data) => console.log(data),
+        (err) => console.log(err),
+        () => {
+          this.sellerDashboardRequestedOrders();
+          console.log("Order Status Modified");
+        }
+      );
+    }
+    else this.sellerDashboardorderAction(orderId,status);
+  }
 }
