67ad001b563db132b9fbba0c620c18b3bbd8a1d9
Rahul <rahul.doit53@gmail.com>
2020-02-09 19:54:01 +0530

    Updated for deployment

--- src/app/services/api-service/auth.interceptor.ts
+++ src/app/services/api-service/auth.interceptor.ts
@@ -21,6 +21,7 @@ export class MetdineInterceptor implements HttpInterceptor{
     const authenticatedRequest = this.Auth.Auth_T ? request.clone({
       setHeaders : {
         Authorization : `Token ${this.Auth.Auth_T}`,
+        "Access-Control-Allow-Origin" : '*',
       }
       })
       : request.clone({
