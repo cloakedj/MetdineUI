f5bf801136b9039cfbc8f19bbe64bb7ea68c0176
Dhananjaykanwar <kanwardhananjaysingh0545@gmail.com>
2020-02-13 11:46:48 +0530

    Update buyer-profile.component.ts

--- src/app/components/user-side/buyer-profile/buyer-profile.component.ts
+++ src/app/components/user-side/buyer-profile/buyer-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fader } from 'src/app/animations/route-animation';
+import { ApiService } from 'src/app/services/api-service/api.service';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-buyer-profile',
@@ -10,8 +12,16 @@ import { fader } from 'src/app/animations/route-animation';
   ]
 })
 export class BuyerProfileComponent implements OnInit {
-
-  constructor() { }
+  userDetailsObs$ : Observer<any>;
+  userProfileData : any;
+  constructor(private api : ApiService) {
+    this.userDetailsObs$ = {
+      next : (data) => this.userProfileData = data,
+      error : err => console.log(err),
+      complete : () => console.log("Completed request to user profile")
+    }
+    this.api.getUserProfileInfo().subscribe(this.userDetailsObs$);
+   }
 
   ngOnInit() {
   }
